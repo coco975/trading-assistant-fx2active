0b83bb8e424b8bb1b925280bc47d07617aed27aa
coco975 <117895571+coco975@users.noreply.github.com>
2026-09-14 15:46:46 +0100

    Wire buy and sell toggles to runtime settings

--- web/app.js
+++ web/app.js
@@ -1,7 +1,8 @@
 const ids = [
-  'trading_enabled','fib_enabled','fib_retracement','stop_buffer_pips','take_profit_mode',
-  'entry_trigger','max_open_positions','support_resistance','previous_swing','trendline',
-  'psychological_level','candle_confirmation','min_confirmations'
+  'trading_enabled','allow_buys','allow_sells','fib_enabled','fib_retracement',
+  'stop_buffer_pips','take_profit_mode','entry_trigger','max_open_positions',
+  'support_resistance','previous_swing','trendline','psychological_level',
+  'candle_confirmation','min_confirmations'
 ];
 
 const $ = (id) => document.getElementById(id);
@@ -15,6 +16,8 @@ function mark(message, kind='ok') {
 
 function setForm(s) {
   $('trading_enabled').checked = s.trading_enabled;
+  $('allow_buys').checked = s.allow_buys;
+  $('allow_sells').checked = s.allow_sells;
   $('fib_enabled').checked = s.fib_enabled;
   $('fib_retracement').value = s.fib_retracement;
   $('stop_buffer_pips').value = s.stop_buffer_pips;
@@ -32,6 +35,8 @@ function setForm(s) {
 function getForm() {
   return {
     trading_enabled: $('trading_enabled').checked,
+    allow_buys: $('allow_buys').checked,
+    allow_sells: $('allow_sells').checked,
     timeframe: 'M15',
     fib_enabled: $('fib_enabled').checked,
     fib_retracement: Number($('fib_retracement').value),
